5ce233b233021ea02c77170d24ae391bcb61d59c
dinhdungweb <dungmaster7@gmail.com>
2026-04-09 17:17:22 +0700

    Add missing useMemo import to React declaration

--- app/routes/app.groups._index.jsx
+++ app/routes/app.groups._index.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useMemo } from "react";
 import { json } from "@remix-run/node";
 import { useLoaderData, useSubmit, useNavigation, useActionData, Link } from "@remix-run/react";
 import {
